046ae2a7302b420976ddf32a89aa0cdf784a414b
Francisco Aguilera <falven2000@hotmail.com>
2017-02-06 22:46:03 -0500

    nested connection calls

--- libs/dapps/irc/test/connection_test.cpp
+++ libs/dapps/irc/test/connection_test.cpp
@@ -12,29 +12,34 @@ int main(int argc, const char * argv [])
 		boost::asio::io_service io_service;
 		// Prevent the io service's run loop from ending before async calls.
 		boost::asio::io_service::work work(io_service);
-		// Run io service even processing loop on another thread.
+		// Run io service event processing loop on another thread.
 		std::thread io_thread([&io_service]() { io_service.run(); });
 
 		dapps::irc::connection con(io_service);
 
-		con.establish_async("irc.deltaanime.net", "6667",
-			[](const boost::asio::ip::tcp::resolver::iterator & iterator)
+		std::string domain("irc.deltaanime.net");
+		std::string port("6667");
+
+		std::cout << "Connecting to: " << domain << ":" << port << std::endl;
+
+		con.establish_async(domain, port,
+			[&con](const boost::asio::ip::tcp::resolver::iterator & iterator)
 		{
 			std::cout << "Connected to: " << iterator->host_name() << std::endl;
-		});
 
-		con.read_async([](const std::string & read)
-		{
-			std::cout << "Received: " << read << std::endl;
-		});
+			auto sent_callback = [](const std::string & message)
+			{
+				std::cout << "Sent: " << message << std::endl;
+			};
+			con.write_async("PASS \r\n", sent_callback);
+			con.write_async("NICK dapps\r\n", sent_callback);
+			con.write_async("USER dapps 8 * :dapps\r\n", sent_callback);
 
-		auto sent_callback = [](const std::string & message)
-		{
-			std::cout << "Sent: " << message << std::endl;
-		};
-		con.write_async("PASS \r\n", sent_callback);
-		con.write_async("NICK dapps\r\n", sent_callback);
-		con.write_async("USER dapps 8 * :dapps\r\n", sent_callback);
+			con.read_async([](const std::string & read)
+			{
+				std::cout << "Received: " << read << std::endl;
+			});
+		});
 
 		std::cin >> std::string();
 
